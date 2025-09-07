f210d37a4f6aa02d9481f1f969018fcad7cbf326
Jean-Philippe LeBlanc <jp.leblanc@gmail.com>
2025-09-07 10:24:37 -0400

    Add/update pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Home = () => {
+    return (
+        <div>
+            <Head>
+                <title>Homepage</title>
+                <meta name="description" content="Welcome to the Homepage" />
+            </Head>
+            <h1>Welcome to the Homepage</h1>
+        </div>
+    );
+};
+
+export default Home;
