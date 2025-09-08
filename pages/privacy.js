cb401aa0921585eea450f779296e2e1415079d05
Jean-Philippe LeBlanc <jp.leblanc@gmail.com>
2025-09-08 12:58:37 -0400

    Add/update pages/privacy.js

---
+++ pages/privacy.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Privacy = () => {
+    return (
+        <div>
+            <h1>Privacy Policy</h1>
+            <p>This is where you can explain your privacy policy in detail.</p>
+        </div>
+    );
+};
+
+export default Privacy;
